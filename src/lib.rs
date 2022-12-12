5c45052a03f0930e20d6ceec15672b1d45597361
thenorili <norilimail@gmail.com>
2022-12-12 18:44:42 -0500

    broke the suit enum -- tired and failed again with phf
    ongoing efforts to figuring out a compile-time kv map here :')

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 // #![feature(const_char_convert)]
 // #![feature(const_option)]
 
-pub mod tiles {
+pub mod mj {
     /*
      * Mahjong is a tile game. This module seeks to provide
      * data structures, constants, and functions
@@ -20,13 +20,13 @@ pub mod tiles {
      */
 
     #[repr(u8)]
-    #[derive(Copy, Clone, Debug, PartialEq)]
+    #[derive(Copy, Clone, Debug, const PartialEq)]
     enum Suit {
         Man    = b'm',
         Pin    = b'p',
         Sou    = b's',
         Honor  = b'z',
-    }
+    };
 
     impl From<char> for Suit {
         fn from(c: char) -> Suit {
@@ -59,10 +59,8 @@ pub mod tiles {
         }
     }
 
-
     impl std::str::FromStr for Suit {
         type Err = ();
-
         fn from_str(s: &str) -> Result<Suit, ()> {
             match s {
                 "m" => Ok(Suit::Man),
@@ -76,82 +74,83 @@ pub mod tiles {
 
     #[derive(Clone)]
     struct TileId {
-        suit: Suit;
-        face: u8;
+        suit: Suit,
+        face: u8,
     }
 
     #[derive(Clone)]
-    struct Tile_Info {
+    struct TileInfo {
         // name might unnecessary...
         // is only kinda useful for honors, and then can be handled other ways
-        name: &'static str;
-        unicode: char;
-        suit: char;
-        face: u8;
+        name: &'static str,
+        unicode: char,
+        suit: char,
+        face: u8,
     }
+
 
     // lnori 2022-12-12
     // this could be put in an impl block with related constants and functions
-    static MJ_TILES: phf::Map<u8, Tile_Info> = {
+    static MJ_TILES: phf::Map<u8, TileInfo> = phf_map! {
         // Man 1-9
-        0x01  => { "1m", '\u{1F007}', 'm', 1},
-        0x02  => { "2m", '\u{1F008}', 'm', 2},
-        0x03  => { "3m", '\u{1F009}', 'm', 3},
-        0x04  => { "4m", '\u{1F00A}', 'm', 4},
-        0x05  => { "5m", '\u{1F00B}', 'm', 5},
+        01  => { "1m", '\u{1F007}', 'm', 1},
+        02  => { "2m", '\u{1F008}', 'm', 2},
+        03  => { "3m", '\u{1F009}', 'm', 3},
+        04  => { "4m", '\u{1F00A}', 'm', 4},
+        05  => { "5m", '\u{1F00B}', 'm', 5},
         // fixme -- indicate these are red somehow
-        0x105 => { "5m", '\u{1F00B}', 'm', 5},
-        0x06  => { "6m", '\u{1F00C}', 'm', 6},
-        0x07  => { "7m", '\u{1F00D}', 'm', 7},
-        0x08  => { "8m", '\u{1F00E}', 'm', 8},
-        0x09  => { "9m", '\u{1F00F}', 'm', 9},
+        105 => { "5m", '\u{1F00B}', 'm', 5},
+        06  => { "6m", '\u{1F00C}', 'm', 6},
+        07  => { "7m", '\u{1F00D}', 'm', 7},
+        08  => { "8m", '\u{1F00E}', 'm', 8},
+        09  => { "9m", '\u{1F00F}', 'm', 9},
         // Pin 1-9
-        0x11  => { "1p", '\u{1F019}', 'p', 1},
-        0x12  => { "2p", '\u{1F01A}', 'p', 2},
-        0x13  => { "3p", '\u{1F01B}', 'p', 3},
-        0x14  => { "4p", '\u{1F01C}', 'p', 4},
-        0x15  => { "5p", '\u{1F01D}', 'p', 5},
+        11  => { "1p", '\u{1F019}', 'p', 1},
+        12  => { "2p", '\u{1F01A}', 'p', 2},
+        13  => { "3p", '\u{1F01B}', 'p', 3},
+        14  => { "4p", '\u{1F01C}', 'p', 4},
+        15  => { "5p", '\u{1F01D}', 'p', 5},
         // fixme -- indicate these are red somehow
-        0x115 => { "5p", '\u{1F01D}', 'p', 5},
-        0x16  => { "6p", '\u{1F01E}', 'p', 6},
-        0x17  => { "7p", '\u{1F01F}', 'p', 7},
-        0x18  => { "8p", '\u{1F020}', 'p', 8},
-        0x19  => { "9p", '\u{1F021}', 'p', 9},
+        115 => { "5p", '\u{1F01D}', 'p', 5},
+        16  => { "6p", '\u{1F01E}', 'p', 6},
+        17  => { "7p", '\u{1F01F}', 'p', 7},
+        18  => { "8p", '\u{1F020}', 'p', 8},
+        19  => { "9p", '\u{1F021}', 'p', 9},
         // Sou 1-9
-        0x21  => { "1s", '\u{1F010}', 's', 1},
-        0x22  => { "2s", '\u{1F011}', 's', 2},
-        0x23  => { "3s", '\u{1F012}', 's', 3},
-        0x24  => { "4s", '\u{1F013}', 's', 4},
-        0x25  => { "5s", '\u{1F014}', 's', 5},
+        21  => { "1s", '\u{1F010}', 's', 1},
+        22  => { "2s", '\u{1F011}', 's', 2},
+        23  => { "3s", '\u{1F012}', 's', 3},
+        24  => { "4s", '\u{1F013}', 's', 4},
+        25  => { "5s", '\u{1F014}', 's', 5},
         // fixme -- indicate these are red somehow
-        0x125 => { "5s", '\u{1F014}', 's', 5},
-        0x26  => { "6s", '\u{1F015}', 's', 6},
-        0x27  => { "7s", '\u{1F016}', 's', 7},
-        0x28  => { "8s", '\u{1F017}', 's', 8},
-        0x29  => { "9s", '\u{1F018}', 's', 9},
+        125 => { "5s", '\u{1F014}', 's', 5},
+        26  => { "6s", '\u{1F015}', 's', 6},
+        27  => { "7s", '\u{1F016}', 's', 7},
+        28  => { "8s", '\u{1F017}', 's', 8},
+        29  => { "9s", '\u{1F018}', 's', 9},
         // Honors
-        0x31 => { "East Wind",  '\u{1F000}', 'z' , 1},
-        0x41 => { "South Wind", '\u{1F001}', 'z', 2 },
-        0x51 => { "West Wind", '\u{1F002}', 'z', 3},
-        0x61 => { "North Wind", '\u{1F003}', 'z', 4},
-        0x71 => { "Red Dragon", '\u{1F004}', 'z', 5},
-        0x81 => { "Green Dragon", '\u{1F005}', 'z', 6},
-        0x91 => { "White Dragon", '\u{1F006}', 'z', 7},
+        31 => { "East Wind",  '\u{1F000}', 'z' , 1},
+        41 => { "South Wind", '\u{1F001}', 'z', 2 },
+        51 => { "West Wind", '\u{1F002}', 'z', 3},
+        61 => { "North Wind", '\u{1F003}', 'z', 4},
+        71 => { "Red Dragon", '\u{1F004}', 'z', 5},
+        81 => { "Green Dragon", '\u{1F005}', 'z', 6},
+        91 => { "White Dragon", '\u{1F006}', 'z', 7},
     };
 
     const fn TileCode_To_TileId (code: u8) -> TileId
     {
         // FIXME: can't use get(code) in constant expression? investigate
-        const suit: Suit = Suit(MJ_TILES.get(code).suit);
-        const face: u8 = MJ_TILES.get(code).face;
+        const tile: TileInfo = MJ_TILES[code];
+        const suit: Suit = Suit(tile.suit);
+        const face: u8 = tile.face;
         return TileId(suit, face);
     }
 
     const fn DoraIndicatorFlow (id: TileId) -> TileId
     {
-        const id: TileId = TileCode_To_Tileid(id);
         if id.suit == Suit::Honor {
-            let const u8 face = match id.face {
+            const face: u8 = match id.face {
                 // E->S->W->N->E
                 1 => 2,
                 2 => 3,
@@ -163,9 +162,9 @@ pub mod tiles {
                 7 => 6,
                 _ => panic!("Invalid tile"),
             };
-            return const TileId(id.suit, face);
+            return TileId(id.suit, face);
         } else {
-            let const u8 face = match id.face {
+            const face: u8 = match id.face {
                 1 => 2,
                 2 => 3,
                 3 => 4,
@@ -177,100 +176,100 @@ pub mod tiles {
                 9 => 1,
                 _ => panic!(),
             };
-            return const TileId(id.suit, face);
+            return TileId(id.suit, face);
         }
     }
 
 
-    type Mahjong_Tileset = [u8; 136]
+    type MahjongTileset = [u8; 136];
     /*
-    static MAHJONG_TILESET_UNSORTED_REDFIVE: Mahjong_Tileset = [
-        // 0x01-0x09 1-9m
-        0x01, 0x01, 0x01, 0x01,
-        0x02, 0x02, 0x02, 0x02,
-        0x03, 0x03, 0x03, 0x03,
-        0x04, 0x04, 0x04, 0x04,
-        0x05, 0x05, 0x05, 0x105, // 0x105 0m
-        0x06, 0x06, 0x06, 0x06,
-        0x07, 0x07, 0x07, 0x07,
-        0x08, 0x08, 0x08, 0x08,
-        0x09, 0x09, 0x09, 0x09,
+    static MAHJONG_TILESET_UNSORTED_REDFIVE: MahjongTileset = [
+        // 01-09 1-9m
+        01, 01, 01, 01,
+        02, 02, 02, 02,
+        03, 03, 03, 03,
+        04, 04, 04, 04,
+        05, 05, 05, 105, // 105 0m
+        06, 06, 06, 06,
+        07, 07, 07, 07,
+        08, 08, 08, 08,
+        09, 09, 09, 09,
 
-        // 0x11-0x19 1-9p
-        0x11, 0x11, 0x11, 0x11,
-        0x12, 0x12, 0x12, 0x12,
-        0x13, 0x13, 0x13, 0x13,
-        0x14, 0x14, 0x14, 0x14,
-        0x15, 0x15, 0x15, 0x115, // 0x115 0p
-        0x16, 0x16, 0x16, 0x16,
-        0x17, 0x17, 0x17, 0x17,
-        0x18, 0x18, 0x18, 0x18,
-        0x19, 0x19, 0x19, 0x19,
+        // 11-19 1-9p
+        11, 11, 11, 11,
+        12, 12, 12, 12,
+        13, 13, 13, 13,
+        14, 14, 14, 14,
+        15, 15, 15, 115, // 115 0p
+        16, 16, 16, 16,
+        17, 17, 17, 17,
+        18, 18, 18, 18,
+        19, 19, 19, 19,
 
-        // 0x21-0x29 1-9s
-        0x21, 0x21, 0x21, 0x21,
-        0x22, 0x22, 0x22, 0x22,
-        0x23, 0x23, 0x23, 0x23,
-        0x24, 0x24, 0x24, 0x24,
-        0x25, 0x25, 0x25, 0x125, // 0x125 0s
-        0x26, 0x26, 0x26, 0x26,
-        0x27, 0x27, 0x27, 0x27,
-        0x28, 0x28, 0x28, 0x28,
-        0x29, 0x29, 0x29, 0x29,
+        // 21-29 1-9s
+        21, 21, 21, 21,
+        22, 22, 22, 22,
+        23, 23, 23, 23,
+        24, 24, 24, 24,
+        25, 25, 25, 125, // 125 0s
+        26, 26, 26, 26,
+        27, 27, 27, 27,
+        28, 28, 28, 28,
+        29, 29, 29, 29,
 
         // honors
-        0x31, 0x31, 0x31, 0x31,
-        0x41, 0x41, 0x41, 0x41,
-        0x51, 0x51, 0x51, 0x51,
-        0x61, 0x61, 0x61, 0x61,
-        0x71, 0x71, 0x71, 0x71,
-        0x81, 0x81, 0x81, 0x81,
-        0x91, 0x91, 0x91, 0x91,
+        31, 31, 31, 31,
+        41, 41, 41, 41,
+        51, 51, 51, 51,
+        61, 61, 61, 61,
+        71, 71, 71, 71,
+        81, 81, 81, 81,
+        91, 91, 91, 91,
     ];
     */
 
-    static MAHJONG_TILESET_UNSORTED_NORED: Mahjong_Tileset = [
-        // 0x01-0x09 1-9m
-        0x01, 0x01, 0x01, 0x01,
-        0x02, 0x02, 0x02, 0x02,
-        0x03, 0x03, 0x03, 0x03,
-        0x04, 0x04, 0x04, 0x04,
-        0x05, 0x05, 0x05, 0x05,
-        0x06, 0x06, 0x06, 0x06,
-        0x07, 0x07, 0x07, 0x07,
-        0x08, 0x08, 0x08, 0x08,
-        0x09, 0x09, 0x09, 0x09,
+    static MAHJONG_TILESET_UNSORTED_NORED: MahjongTileset = [
+        // 01-09 1-9m
+        01, 01, 01, 01,
+        02, 02, 02, 02,
+        03, 03, 03, 03,
+        04, 04, 04, 04,
+        05, 05, 05, 05,
+        06, 06, 06, 06,
+        07, 07, 07, 07,
+        08, 08, 08, 08,
+        09, 09, 09, 09,
 
-        // 0x11-0x19 1-9p
-        0x11, 0x11, 0x11, 0x11,
-        0x12, 0x12, 0x12, 0x12,
-        0x13, 0x13, 0x13, 0x13,
-        0x14, 0x14, 0x14, 0x14,
-        0x15, 0x15, 0x15, 0x15,
-        0x16, 0x16, 0x16, 0x16,
-        0x17, 0x17, 0x17, 0x17,
-        0x18, 0x18, 0x18, 0x18,
-        0x19, 0x19, 0x19, 0x19,
+        // 11-19 1-9p
+        11, 11, 11, 11,
+        12, 12, 12, 12,
+        13, 13, 13, 13,
+        14, 14, 14, 14,
+        15, 15, 15, 15,
+        16, 16, 16, 16,
+        17, 17, 17, 17,
+        18, 18, 18, 18,
+        19, 19, 19, 19,
 
-        // 0x21-0x29 1-9s
-        0x21, 0x21, 0x21, 0x21,
-        0x22, 0x22, 0x22, 0x22,
-        0x23, 0x23, 0x23, 0x23,
-        0x24, 0x24, 0x24, 0x24,
-        0x25, 0x25, 0x25, 0x25,
-        0x26, 0x26, 0x26, 0x26,
-        0x27, 0x27, 0x27, 0x27,
-        0x28, 0x28, 0x28, 0x28,
-        0x29, 0x29, 0x29, 0x29,
+        // 21-29 1-9s
+        21, 21, 21, 21,
+        22, 22, 22, 22,
+        23, 23, 23, 23,
+        24, 24, 24, 24,
+        25, 25, 25, 25,
+        26, 26, 26, 26,
+        27, 27, 27, 27,
+        28, 28, 28, 28,
+        29, 29, 29, 29,
 
         // honors
-        0x31, 0x31, 0x31, 0x31,
-        0x41, 0x41, 0x41, 0x41,
-        0x51, 0x51, 0x51, 0x51,
-        0x61, 0x61, 0x61, 0x61,
-        0x71, 0x71, 0x71, 0x71,
-        0x81, 0x81, 0x81, 0x81,
-        0x91, 0x91, 0x91, 0x91,
+        31, 31, 31, 31,
+        41, 41, 41, 41,
+        51, 51, 51, 51,
+        61, 61, 61, 61,
+        71, 71, 71, 71,
+        81, 81, 81, 81,
+        91, 91, 91, 91,
     ];
 
     // i don't like that it mismatches the index...
@@ -278,48 +277,6 @@ pub mod tiles {
     // and add it to the static array at the top.
     //
     // quarantine --
-    enum TileCode {
-        M0 = 0x105,
-        M1 = 0x01,
-        M2 = 0x02,
-        M3 = 0x03,
-        M4 = 0x04,
-        M5 = 0x05,
-        M6 = 0x06,
-        M7 = 0x07,
-        M8 = 0x08,
-        M9 = 0x09,
-
-        P0 = 0x115,
-        P1 = 0x11,
-        P2 = 0x12,
-        P3 = 0x13,
-        P4 = 0x14,
-        P5 = 0x15,
-        P6 = 0x16,
-        P7 = 0x17,
-        P8 = 0x18,
-        P9 = 0x19,
-
-        S0 = 0x125,
-        S1 = 0x21,
-        S2 = 0x22,
-        S3 = 0x23,
-        S4 = 0x24,
-        S5 = 0x25,
-        S6 = 0x26,
-        S7 = 0x27,
-        S8 = 0x28,
-        S9 = 0x29,
-
-        Z1 = 0x31,
-        Z2 = 0x41,
-        Z3 = 0x51,
-        Z4 = 0x61,
-        Z5 = 0x71,
-        Z6 = 0x81,
-        Z7 = 0x91,
-    }
     const fn try_tilecode_to_tile(code: TileCode) -> Result<Tile, ()> {
         let val = code as usize;
         let m: Suit = Suit::Man;
@@ -328,46 +285,46 @@ pub mod tiles {
         let z: Suit = Suit::Honor;
 
         let result = match val  {
-            0x105 => Tile { suit: m, face: 0 },
-            0x01 => Tile { suit: m, face: 1 },
-            0x02 => Tile { suit: m, face: 2 },
-            0x03 => Tile { suit: m, face: 3 },
-            0x04 => Tile { suit: m, face: 4 },
-            0x05 => Tile { suit: m, face: 5 },
-            0x06 => Tile { suit: m, face: 6 },
-            0x07 => Tile { suit: m, face: 7 },
-            0x08 => Tile { suit: m, face: 8 },
-            0x09 => Tile { suit: m, face: 9 },
+            105 => Tile { suit: m, face: 0 },
+            01 => Tile { suit: m, face: 1 },
+            02 => Tile { suit: m, face: 2 },
+            03 => Tile { suit: m, face: 3 },
+            04 => Tile { suit: m, face: 4 },
+            05 => Tile { suit: m, face: 5 },
+            06 => Tile { suit: m, face: 6 },
+            07 => Tile { suit: m, face: 7 },
+            08 => Tile { suit: m, face: 8 },
+            09 => Tile { suit: m, face: 9 },
 
-            0x115 => Tile { suit: p, face: 0 },
-            0x11 => Tile { suit: p, face: 1 },
-            0x12 => Tile { suit: p, face: 2 },
-            0x13 => Tile { suit: p, face: 3 },
-            0x14 => Tile { suit: p, face: 4 },
-            0x15 => Tile { suit: p, face: 5 },
-            0x16 => Tile { suit: p, face: 6 },
-            0x17 => Tile { suit: p, face: 7 },
-            0x18 => Tile { suit: p, face: 8 },
-            0x19 => Tile { suit: p, face: 9 },
+            115 => Tile { suit: p, face: 0 },
+            11 => Tile { suit: p, face: 1 },
+            12 => Tile { suit: p, face: 2 },
+            13 => Tile { suit: p, face: 3 },
+            14 => Tile { suit: p, face: 4 },
+            15 => Tile { suit: p, face: 5 },
+            16 => Tile { suit: p, face: 6 },
+            17 => Tile { suit: p, face: 7 },
+            18 => Tile { suit: p, face: 8 },
+            19 => Tile { suit: p, face: 9 },
 
-            0x125 => Tile { suit: s, face: 0 },
-            0x21 => Tile { suit: s, face: 1 },
-            0x22 => Tile { suit: s, face: 2 },
-            0x23 => Tile { suit: s, face: 3 },
-            0x24 => Tile { suit: s, face: 4 },
-            0x25 => Tile { suit: s, face: 5 },
-            0x26 => Tile { suit: s, face: 6 },
-            0x27 => Tile { suit: s, face: 7 },
-            0x28 => Tile { suit: s, face: 8 },
-            0x29 => Tile { suit: s, face: 9 },
+            125 => Tile { suit: s, face: 0 },
+            21 => Tile { suit: s, face: 1 },
+            22 => Tile { suit: s, face: 2 },
+            23 => Tile { suit: s, face: 3 },
+            24 => Tile { suit: s, face: 4 },
+            25 => Tile { suit: s, face: 5 },
+            26 => Tile { suit: s, face: 6 },
+            27 => Tile { suit: s, face: 7 },
+            28 => Tile { suit: s, face: 8 },
+            29 => Tile { suit: s, face: 9 },
 
-            0x31 => Tile { suit: z, face: 1 },
-            0x41 => Tile { suit: z, face: 2 },
-            0x51 => Tile { suit: z, face: 3 },
-            0x61 => Tile { suit: z, face: 4 },
-            0x71 => Tile { suit: z, face: 5 },
-            0x81 => Tile { suit: z, face: 6 },
-            0x91 => Tile { suit: z, face: 7 },
+            31 => Tile { suit: z, face: 1 },
+            41 => Tile { suit: z, face: 2 },
+            51 => Tile { suit: z, face: 3 },
+            61 => Tile { suit: z, face: 4 },
+            71 => Tile { suit: z, face: 5 },
+            81 => Tile { suit: z, face: 6 },
+            91 => Tile { suit: z, face: 7 },
             _    => return Err(())
         };
         Ok(result)
@@ -381,47 +338,47 @@ pub mod tiles {
     }
 
     static TILESET: [usize; 136] = [
-        // 0x01-0x09 1-9m
-        0x01, 0x01, 0x01, 0x01,
-        0x02, 0x02, 0x02, 0x02,
-        0x03, 0x03, 0x03, 0x03,
-        0x04, 0x04, 0x04, 0x04,
-        0x05, 0x05, 0x05, 0x105, // 0x105 0m
-        0x06, 0x06, 0x06, 0x06,
-        0x07, 0x07, 0x07, 0x07,
-        0x08, 0x08, 0x08, 0x08,
-        0x09, 0x09, 0x09, 0x09,
+        // 01-09 1-9m
+        01, 01, 01, 01,
+        02, 02, 02, 02,
+        03, 03, 03, 03,
+        04, 04, 04, 04,
+        05, 05, 05, 105, // 105 0m
+        06, 06, 06, 06,
+        07, 07, 07, 07,
+        08, 08, 08, 08,
+        09, 09, 09, 09,
 
-        // 0x11-0x19 1-9p
-        0x11, 0x11, 0x11, 0x11,
-        0x12, 0x12, 0x12, 0x12,
-        0x13, 0x13, 0x13, 0x13,
-        0x14, 0x14, 0x14, 0x14,
-        0x15, 0x15, 0x15, 0x115, // 0x115 0p
-        0x16, 0x16, 0x16, 0x16,
-        0x17, 0x17, 0x17, 0x17,
-        0x18, 0x18, 0x18, 0x18,
-        0x19, 0x19, 0x19, 0x19,
+        // 11-19 1-9p
+        11, 11, 11, 11,
+        12, 12, 12, 12,
+        13, 13, 13, 13,
+        14, 14, 14, 14,
+        15, 15, 15, 115, // 115 0p
+        16, 16, 16, 16,
+        17, 17, 17, 17,
+        18, 18, 18, 18,
+        19, 19, 19, 19,
 
-        // 0x21-0x29 1-9s
-        0x21, 0x21, 0x21, 0x21,
-        0x22, 0x22, 0x22, 0x22,
-        0x23, 0x23, 0x23, 0x23,
-        0x24, 0x24, 0x24, 0x24,
-        0x25, 0x25, 0x25, 0x125, // 0x125 0s
-        0x26, 0x26, 0x26, 0x26,
-        0x27, 0x27, 0x27, 0x27,
-        0x28, 0x28, 0x28, 0x28,
-        0x29, 0x29, 0x29, 0x29,
+        // 21-29 1-9s
+        21, 21, 21, 21,
+        22, 22, 22, 22,
+        23, 23, 23, 23,
+        24, 24, 24, 24,
+        25, 25, 25, 125, // 125 0s
+        26, 26, 26, 26,
+        27, 27, 27, 27,
+        28, 28, 28, 28,
+        29, 29, 29, 29,
 
         // honors
-        0x31, 0x31, 0x31, 0x31,
-        0x41, 0x41, 0x41, 0x41,
-        0x51, 0x51, 0x51, 0x51,
-        0x61, 0x61, 0x61, 0x61,
-        0x71, 0x71, 0x71, 0x71,
-        0x81, 0x81, 0x81, 0x81,
-        0x91, 0x91, 0x91, 0x91,
+        31, 31, 31, 31,
+        41, 41, 41, 41,
+        51, 51, 51, 51,
+        61, 61, 61, 61,
+        71, 71, 71, 71,
+        81, 81, 81, 81,
+        91, 91, 91, 91,
     ];
 
     /*
